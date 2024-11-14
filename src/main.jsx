1b5e7601e41e5e50ae18836f1952a4e7ad88aab9
raghavendragouda775 <raghudbose@gmail.com>
2024-11-14 22:40:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx'
-export const server='https://elearning-kaqq.onrender.com'
+export const server='https://elearning-frontend-x1kn.vercel.app'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserContextProvider>
